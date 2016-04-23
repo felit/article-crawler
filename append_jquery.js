f868af87546aea306d88952ee382d7776cbfe2b0
felit <felit123456>
2016-04-23 19:06:43 +0800

    添加jquery工具

---
+++ append_jquery.js
@@ -0,0 +1,8 @@
+/***
+ * 在页面中添加jquery脚本
+ * @type {Element}
+ */
+script = document.createElement('script');
+script.src = '//cdn.bootcss.com/jquery/2.2.1/jquery.min.js';
+script.type = "text/javascript";
+document.body.appendChild(a);
---
+++ js/config.js
@@ -0,0 +1,11 @@
+/**
+ * 配置信息
+ */
+(function (win) {
+    var config = {
+        'save_url': function () {
+            return 'http://localhost/dede/article_add.php';
+        }
+    };
+    win.config = Config;
+})(win);
